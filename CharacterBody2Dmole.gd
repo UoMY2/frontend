8c3a8e99be9e981ad4f8266a1103232e4b6c5134
Elias Liassides <elias@Eliass-MacBook-Pro.local>
2024-02-16 00:40:58 +0000

    last commit

--- CharacterBody2Dmole.gd
+++ CharacterBody2Dmole.gd
@@ -1,27 +1,8 @@
-extends CharacterBody2D
+extends Node2D
+
+var timer : Timer
 
 
-var speed = 200
-
-func _ready():
-	# Hide the mole initially
-	_hide()
-
-func _physics_process(delta):
-	move_and_slide()
-
-func _on_CollisionShape2D_area_entered(area):
-	# Show the mole when it is clicked
-	_show()
-
-func _hide():
-	# Hide the mole by setting its position below the screen
-	position.y = 1000
-
-func _show():
-	# Show the mole by setting its position on the screen
-	position.y = 0
-
-func _on_Mole_hidden():
-	# Free the mole object from memory when it is hidden
+func _on_timer_timeout():
 	queue_free()
+	get_tree().quit()
